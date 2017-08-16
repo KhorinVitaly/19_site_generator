2472185552212396e1fb91431fe0afbf1831a0c6
KhorinVitaly <khorin.v@gmail.com>
2017-08-16 19:27:32 +0600

    first commit, realize task

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # Encyclopedia
 
-[TODO. There will be project description]
+This repo illustrate how python script can generate static web site from markdown articles. 
+It's just for education, script will work with directory structure only like in repo.
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+# Quickstart
+
+Launch script in terminal  
+
+```bash
+python3 generator.py 
+[I 170816 19:14:17 server:283] Serving on http://127.0.0.1:5500
+[I 170816 19:14:17 handlers:60] Start watching changes
+[I 170816 19:14:17 handlers:62] Start detecting changes
+```
+
+After that you can browse http://127.0.0.1:5500 and test how pages change if you change articles or teamplates. 
+ 
+Else site published on GitHub Pages you can look it on https://khorinvitaly.github.io 
 
 # Project Goals
 
---
+++ generator.py
@@ -0,0 +1,60 @@
+from jinja2 import Template
+import json
+import markdown
+import re
+from livereload import Server
+
+
+def make_site():
+    config_dict = fetch_config()
+    topics = config_dict['topics']
+    articles = config_dict['articles']
+    article_page_template_text = load_file('templates/article_template.html')
+
+    for article in articles:
+        article_md_text = load_file('articles/' + article['source'])
+        article_page_text = generate_article_page(article, article_md_text, article_page_template_text)
+        re_result = re.search(r'\w+.(\w+)', article['source'])
+        file_name = re_result.group(1)
+        path_to_html = file_name + '.html'
+        write_to_file(article_page_text, 'site/' + path_to_html)
+        article['html_source'] = path_to_html
+
+    index_page_template_text = load_file('templates/index_template.html')
+    index_page_text = generate_index_page(topics, articles, index_page_template_text)
+    write_to_file(index_page_text, 'site/index.html')
+
+
+def load_file(filepath):
+    with open(filepath, "r", encoding='UTF-8') as current_file:
+        return current_file.read()
+
+
+def fetch_config():
+    json_data = load_file('config.json')
+    return json.loads(json_data)
+
+
+def generate_index_page(topics, articles, template_text):
+    index_template = Template(template_text)
+    for topic in topics:
+        topic['articles'] = [x for x in articles if x['topic'] == topic['slug']]
+    return index_template.render(topics=topics)
+
+
+def generate_article_page(article, article_md_text, template_text):
+    article_template = Template(template_text)
+    article_text = markdown.markdown(article_md_text, extensions=['codehilite'])
+    return article_template.render(acticle_content=article_text, article=article)
+
+
+def write_to_file(html_data, filepath):
+    with open(filepath, "w", encoding='UTF-8') as file_for_write:
+        file_for_write.write(html_data)
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.watch('templates/', make_site)
+    server.watch('articles/', make_site)
+    server.serve(root='site/')
